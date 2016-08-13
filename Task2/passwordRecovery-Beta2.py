acd651a322ab5542846cd6cd385cfe19207ed0ca
zxlim <zxlim920@gmail.com>
2016-08-13 16:55:54 +0800

    Refined some lines of code (BETA) for task 2

--- Task2/passwordRecovery-Beta2.py
+++ Task2/passwordRecovery-Beta2.py
@@ -12,34 +12,26 @@ combinationList = []
 def currentDateTime():
     return unicode(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-def generateCombinations():
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for pwLength in range(1,7,1):
-        for n in permutations(numbers, pwLength):
-            combinationList.append("".join(n[0:]))
-
 def passwordRecovery():
     with open("shadow.txt", "r") as shadowFile:
         for line in shadowFile:
             line = line.replace("\n", "").split(":")
-            username = line[0]
-            encryption = line[1]
-            hashID = encryption.split("$")[1]
-            userSalt = encryption.split("$")[2]
-            salt = "${}${}$".format(hashID, userSalt)
-            print("Attempting recovery on " + username + "'s password...")
-            decrypt(encryption, salt)
+            salt = "${}${}$".format(line[1].split("$")[1], line[1].split("$")[2])
+            print("Attempting recovery on " + line[0] + "'s password...")
+            print("Password found:\t\t" + decrypt(line[1], salt) + "\n")
         shadowFile.close()
 
 def decrypt(encryption, salt):
     for tries in combinationList:
         if crypt(tries, salt) == encryption:
-            print("Password found:\t\t" + tries + "\n")
-            return
+            return tries
 
 def main():
     print("\nProgram start time:\t" + currentDateTime() + "\n")
-    generateCombinations()
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    for pwLength in range(1,7,1):
+        for n in permutations(numbers, pwLength):
+            combinationList.append("".join(n[0:]))
     passwordRecovery()
     print("Program complete time:\t" + currentDateTime() + "\n")
 
